521becc0e3665dfb6eb8c83ff3d183c18a4ab696
Steven Osborne <steven.c.osborne@gmail.com>
2018-09-02 16:22:33 -0400

    Added validation handelers

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 //Local file loads
 
@@ -30,6 +31,24 @@ app.get('/todos', (req,res) => {
     }, (e) => {
         res.status(400).send(e);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  } 
+    
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+    
 });
 
 app.listen(3000, () => {
