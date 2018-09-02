f1f45fafbb8b2efc1de2543e613636d78c7cac1c
Steven Osborne <steven.c.osborne@gmail.com>
2018-09-02 15:02:56 -0400

    Test POST todos route

--- server/models/todo.js
+++ server/models/todo.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var Todo = mongoose.model("Todo",{
     text: {
         type: String,
-        require: true,
+        required: true,
         minlength: 1,
         trim: true,
     },
@@ -17,6 +17,4 @@ var Todo = mongoose.model("Todo",{
     }
 });
 
-module.exports = {
-    Todo: Todo,
-};
+module.exports = {Todo};
