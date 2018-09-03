05dd20b2b16efb705390e4d2332fba2e970d197d
Steven Osborne <steven.c.osborne@gmail.com>
2018-09-03 11:42:44 -0400

    hash user pass before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,30 @@
 const {SHA256} = require('crypto-js');
 
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10,
-};
+var password = '123abc';
+ bcrypt.genSalt(10, (err, salt) => {
+     bcrypt.hash(password, salt, (err, hash) => {
+         console.log(hash);
+     });
+ });
 
-var token = jwt.sign(data,'123abc');
-console.log(token);
+var hashedPassword = '$2a$10$rAHeK2Fi.X8N1n5vJLWdpubmigVnYlMI8d3KrCin/yJPYC5hkIsI2';
 
-var decoded = jwt.verify(token, '123abc');
-console.log(decoded);
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
+
+//var data = {
+//    id: 10,
+//};
+//
+//var token = jwt.sign(data,'123abc');
+//console.log(token);
+//
+//var decoded = jwt.verify(token, '123abc');
+//console.log(decoded);
 //var message = 'I am steven'
 //
 //var hash = SHA256(message).toString();
